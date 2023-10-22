9ba4152f746d3efd936196d5c64dba4532ccc810
Sagisak <127295204+Sagisak@users.noreply.github.com>
2023-10-22 16:18:21 +0700

    design table sudah
    paling di tweek lagi besokk biar lebih cakep aja

--- UTS_webprog_lec/homepageAdmin.php
+++ UTS_webprog_lec/homepageAdmin.php
@@ -83,27 +83,32 @@ if (mysqli_num_rows($query) == 0) {
   </div>
   </div>';
 } else {
-  echo '<table border="1">';
-  echo '<tr>';
-  echo '<th>No.</th>';
+  echo "<div class='container-xl table-responsive'>";
+  echo "<br/>";
+  echo '<table class="table table-striped table-bordered">';
+  echo '<thead>';
+  echo '<tr class="table-dark ">';
+  echo '<th >No.</th>';
   echo '<th>Name</th>';
   echo '<th>Price</th>';
   echo '<th>Type</th>';
   echo '<th>Description</th>';
-  echo '<th>photo</th>';
-  echo '<th>Hapus</th>';
-  echo '<th>Edit</th>';
+  echo '<th><center>photo</center></th>';
+  echo '<th><center>Hapus</center></th>';
+  echo '<th><center>Edit</center></th>';
   echo '</tr>';
+  echo '</thead>';
   $counter = 1;
+  echo '<tbody class="table-group-divider">';
 while ($hasil = mysqli_fetch_array($query)) {
-  echo "<tr>";
+  echo "<tr class='table-info'>";
   echo "<td>$counter</td>";
   echo "<td>{$hasil['Name']}</td>";
   echo "<td>{$hasil['Price']}</td>";
   echo "<td>{$hasil['Type']}</td>";
   echo "<td>{$hasil['Description']}</td>";
-  echo "<td><img src='./image/{$hasil['Photo']}' style='width: 50px; height: 50px;'></td>";
-  echo '<td><div class="container">
+  echo "<td class='align-middle text-center'><center><img src='./image/{$hasil['Photo']}' style='width: 150px;border: 2px solid #222; padding: 2px;' class='img-fluid rounded'></center></td>";
+  echo '<td class="align-middle text-center"><div class="container">
     <div class="mx-auto">
     <a href="homepageAdmin.php?Name=' . $hasil["Name"] . '">
         <center>
@@ -114,7 +119,7 @@ while ($hasil = mysqli_fetch_array($query)) {
       </a>
     </div>
   </div></td>';
-  echo '<td><div class="container">
+  echo '<td class="align-middle text-center"><div class="container">
     <div class="mx-auto">
     <a href="update.php?Name=' . $hasil["Name"] . '&Price=' . $hasil["Price"] . '&Type=' . $hasil["Type"] . '&Description=' . $hasil["Description"] . '&Photo=' . $hasil["Photo"] . '">
         <center>
@@ -129,11 +134,13 @@ while ($hasil = mysqli_fetch_array($query)) {
   $counter++;
   
 }
-
+echo '</tbody>';
 echo '</table>';
+echo '</div>';
 mysqli_close($con);
-echo '<div class="container">
-<div class="d-flex align-items-center vh-100">
+echo '
+<br/><div class="container">
+<div class="d-flex align-items-center">
   <div class="mx-auto">
     <a href="homepageAdminAddData.php">
       <center>
