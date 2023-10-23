9f0552ddcde23ce41c2bb6647c1d028fe0eb0aaf
Pkoobz <kristoffen.p@gmail.com>
2023-10-23 18:14:06 +0700

    login
    blm selesai yg register css nya bingung

---
+++ UTS_webprog_lec/login/login_user_proses.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once('db.php');
+
+if (isset($_POST['captcha'])) {
+    $userCaptcha = $_POST['captcha'];
+    if ($userCaptcha === $_SESSION['captcha_code']) {
+        echo "CAPTCHA is correct!";
+    } else {
+        echo "CAPTCHA is incorrect. Please try again.";
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email = ? OR username = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->execute([$email, $username]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id']; 
+        header('Location: homescreen.php'); 
+        exit;
+    } else {
+        echo "Invalid email/username or password";
+    }
+}
+?>
--- UTS_webprog_lec/login/register_user.php
+++ UTS_webprog_lec/login/register_user.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,19 +12,19 @@
     <main>
         <div class="signup-form">
             <form action="register_proses.php" method="POST">
-                <label for="firstname">First Name:</label>
+for="firstname">First Na
                 <input type="text" name="firstname" required>
-                <label for="lastname">Last Name:</label>
+for="lastname">Last Na
                 <input type="text" name="lastname" required>
-                <label for="username">Username:</label>
+for="username">Userna
                 <input type="text" name="username" required>
-                <label for="email">Email:</label>
+for="email">Ema
                 <input type="text" name="email" required>
-                <label for="password">Password:</label>
+for="password">Passwo
                 <input type="password" name="password" required>
-                <label for="birthdate">Birthdate:</label>
+for="birthdate">Birthda
                 <input type="date" name="birthdate" required>
-                <label for="gender">Gender:</label>
+for="gender">Gend
                 <select name="gender" required>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
@@ -34,4 +34,67 @@
         </div>
     </main>
 </body>
-</html>
+</html> -->
+
+<head>
+		<meta charset="utf-8">
+   		<meta name="keywords" content="Admin Login By Zaki" >
+    	<meta name="description" content="Admin Login By Zaki" >
+    	<title>Sign In </title>
+		<script src="https://use.fontawesome.com/fbf13eceb7.js"></script>
+    	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<link rel="stylesheet" href="register_user.css" >
+    </head>
+    <body>
+	
+    	<div class="biggest">
+    	
+		
+    		<div class="nav">
+    			<ul>
+			
+    				<li><img src="https://i.imgur.com/cqN7DSd.png" width="70" height="70" /></li>
+    				<li class="move1"><a class="login1" href="login_user.php"><i class="fa fa-user-o" aria-hidden="true"></i> Login</a></li>
+    				<li class="move2"><a class="sign-up" href="#"><i class="fa fa-user-o" aria-hidden="true"></i> Register</a></li>
+					<hr/>
+    			</ul>
+    			
+    			<!-- Start Introduction here-->
+    		<div class="intro">
+    			<div class="par">
+	    			<h3>Welcome To Your account.</h3>
+	    			<span>Designed With love <br>by Kouloughli Hemza</span>
+	    			<div class="social">
+	    				<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+						<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+	    			</div>
+    			</div>
+    		</div>
+    		<!-- End Introduction here-->
+    		</div>
+    		    <div class="container">
+				    <div class="reg">
+	    				<p><span>Register !</span> New account to see what <br>we have for you.</p><br>
+	    				<form action="register_proses.php" method="POST">
+                        <input type="text" name="firstname" placeholder="first name">
+                        <input type="text" name="lastname" placeholder="last name" required>
+                        <input type="text" name="username" placeholder="username" required>
+                        <!-- <input type="text" name="email" placeholder="email" required>
+                        <input type="password" name="password" placeholder="password" required>
+                        <input type="date" name="birthdate" placeholder="birth date" required>
+                        <select name="gender" required>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select> -->
+	    				<!--<span><input type="submit" value=""><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></span>-->
+	    				<button><i class="fa fa-plus" aria-hidden="true"></i></button>
+	    				</form><br>
+	    				<p>Already Have An account ?<span class="login1"><a href="login_user.html"> Log in</a></span></p>
+	    			</div>
+                </div>
+    	</div>
+	</body>
+
+
+    
